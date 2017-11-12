3565e051f6eceb8dc1aa3b424a57f89565a71333
Sumil Majithia <majithia.sumil@gmail.com>
2017-11-12 01:49:45 -0800

    Create Day 5: Loops.cpp

---
+++ Day 5: Loops.cpp
@@ -0,0 +1,33 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    for (int i=0;i<10;i++)
+        printf("%d x %d = %d \n",n,(i+1),n*(i+1));
+    return 0;
+}
